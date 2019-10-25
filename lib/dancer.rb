1c22909ff62eeee675730e85ac297aac57500213
Emery Tate <tateejt@gmail.com>
2019-10-25 05:55:10 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,7 +3,7 @@ require_relative './dance_module.rb'
 class Dancer
   include Dance::FancyDance::InstanceMethods
   extend MetaDancing::FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+
  
   attr_accessor :name
  
